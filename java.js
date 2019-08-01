ff24de1917f831e727222b3962428d78742552b7
hacktjgithubuser <49350842+hacktjgithubuser@users.noreply.github.com>
2019-08-01 09:33:37 -0400

    Add files via upload

---
+++ java.js
@@ -0,0 +1,23 @@
+function homepage() {
+  window.location("helpinghands.html")
+}
+
+function newjob() {
+  window.location("newjob.html")
+}
+
+function pastreq() {
+  window.location(".html")
+}
+
+function profile() {
+  window.location(".html")
+}
+
+function login() {
+  window.location("login.html")
+}
+
+function signup() {
+  window.location(".html")
+}
